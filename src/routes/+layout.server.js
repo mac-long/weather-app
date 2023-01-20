493fad003c61bef0f8e3cbe3f45dd22c6fe0d7af
Mac <mac@3sidedcube.com>
2023-01-20 22:03:24 +0000

    fix(getUserItems): hardcoded id variable for testing'

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,7 +3,7 @@ import { getUser } from 'utilities/user';
 import { getUserItems } from '../lib/utilities/user';
 
 export async function load() {
-	const user = { user: await getUser(1), items: await getUserItems() };
+	const user = { user: await getUser(1), items: await getUserItems(1) };
 	if (user) return user;
 	throw error(404, 'User not found');
 }
