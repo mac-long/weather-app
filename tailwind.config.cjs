8fe1931200e768434f0f51708ed8f5254e42b354
Mac <mac@3sidedcube.com>
2023-01-24 11:47:56 +0000

    feat(Fonts): added poppins and anonymous pro fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,8 @@ module.exports = {
 				secondary: '#7f7f7f'
 			},
 			fontFamily: {
-				poppins: ['Poppins', 'sans-serif'],
-				anonymous: ['Anonymous Pro', 'monospace']
+				heading: ['Poppins', 'sans-serif'],
+				body: ['Anonymous Pro', 'monospace']
 			},
 			boxShadow: {
 				nav: '0px 0px 2px rgba(0, 0, 0, 0.5);'
