46390ef7c7095801cf3eb84d8b8abd2dac082100
Mac <mac@3sidedcube.com>
2023-01-24 18:38:45 +0000

    fix(Header): abstracted to component

---
+++ src/lib/components/layout/header.svelte
@@ -0,0 +1,21 @@
+<header
+	class="flex items-center justify-between w-screen h-32 px-4 pt-16 pb-2 bg-gradient-to-tr from-primary to-primaryDark"
+>
+	<div class="flex items-center w-auto space-x-4">
+		<img class="w-16 h-12" src="/cloudy.webp" alt="" role="presentation" />
+		<h1 class="uppercase">Weather App</h1>
+	</div>
+	<button class="w-auto ghost">
+		<svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g id="Add">
+				<g id="np_add_1776296_FFFFFF">
+					<path
+						id="Path"
+						d="M9.92139 0V9.75H0.171387V14.25H9.92139V24H14.4214V14.25H24.1714V9.75H14.4214V0H9.92139Z"
+						fill="white"
+					/>
+				</g>
+			</g>
+		</svg>
+	</button>
+</header>
