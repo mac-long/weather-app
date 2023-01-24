29f8652a44adc8731e1c9e357f41369d4e52b523
Mac <mac@3sidedcube.com>
2023-01-24 11:37:12 +0000

    feat(Tailwind): theme configuation complete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					light: '#F40256',
+					dark: '#FE00C6'
+				},
+				secondary: '#7f7f7f'
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+				anonymous: ['Anonymous Pro', 'monospace']
+			},
+			boxShadow: {
+				nav: '0px 0px 2px rgba(0, 0, 0, 0.5);'
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/forms')]
 };
