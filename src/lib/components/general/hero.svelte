8380450366df9e5d1843514246908387df449f59
Mac <mac@3sidedcube.com>
2023-01-27 21:09:36 +0000

    ui(Profile): image and form added

--- src/lib/components/general/hero.svelte
+++ src/lib/components/general/hero.svelte
@@ -1,8 +1,13 @@
 <script>
-	export let noColor = false;
+	export let noColor = false,
+		profile = false;
 </script>
 
-<div class="w-full py-4 h-[480px] bg-gradient-to-br from-primaryDark to-primary-500" class:noColor>
+<div
+	class="w-full py-4 h-[480px] bg-gradient-to-br from-primaryDark to-primary-500"
+	class:noColor
+	class:profile
+>
 	<div class="flex flex-col items-center justify-center text-center text-white">
 		<slot />
 	</div>
@@ -10,6 +15,6 @@
 
 <style lang="postcss">
 	.noColor {
-		@apply from-white to-gray-100;
+		@apply from-white to-gray-100 h-auto bg-red-500;
 	}
 </style>
